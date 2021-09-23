55b7e48e7a012d053a691345d1b12ad85d04d650
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2021-09-23 18:42:18 +0200

    Rendering an element to a web page. ReactDOM is the function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const h1 = <h1>Hello World</h1>
-ReactDOM.render(
-  h1, document.getElementById('root')
-);
+const element =( 
+  <div>
+  <li>Item 1</li>
+  <li>Item 1</li>
+  <li>Item 1</li>
+ These two numbers = {5+5}
+  </div>
+  
+  )
+
+ReactDOM.render(element, document.getElementById('root'))
