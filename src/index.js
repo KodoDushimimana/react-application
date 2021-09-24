a7038a462e7ce7da14b189d431045e02f0849439
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2021-09-24 10:42:15 +0200

    Use of State
    State are declared in the constructor function later to be used in render.
    
    "this.state.variable"

--- src/index.js
+++ src/index.js
@@ -3,24 +3,15 @@ import ReactDOM from 'react-dom';
 
 
 
-const a = 'string'
- if(a ===5){
-   console.log(` a is ${a}`)
- }else{
-   console.log('NOthing')
- }
-const element =( 
-  <div>
-    <p>{a}</p>
-    <ul>
-      <li>Item 1</li>
-      <li>Item 1</li>
-      <li>Item 1</li>
-    </ul>
- React is  {5+5} times easy to learn
+class Car extends React.Component{
+  constructor(){
+    super()
+    this.state = {color:'red'}
+  }
+  render(){
+    return <h2>Hi! This is my first React Component! I am hoping the color {this.state.color}</h2>
+  }
+}
 
-  </div>
-  
-  )
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<Car />, document.getElementById('root'))
