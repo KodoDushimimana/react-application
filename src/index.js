b097b16991e6969b671691917effe7ff61fa8bd6
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2021-09-24 16:00:01 +0200

    Props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './Car.js'
+//import Car from '../components/Car.js'
 
 
-class Garage extends React.Component{
-  render(){
-    return <h1> This is a Garage Component</h1>
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: " Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+           {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
   }
 }
 
-
-ReactDOM.render(
-  <Car color="red" model ="Ford"/>,
-  document.getElementById('root')
-)
+ReactDOM.render(<Car/>, document.getElementById('root'))
