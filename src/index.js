9eaa81e22cfe2a96112c67e6c0a09def2e2ea7ef
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2021-09-25 15:14:58 +0200

    Handling form

--- src/index.js
+++ src/index.js
@@ -6,43 +6,15 @@ import ReactDOM from 'react-dom';
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: 'here', number: '' };
+    this.state = {
+      description: 'The content of a textarea goes in the value attribute'
+    };
   }
-  myChangeHandler = (event) => {
-    this.setState({username: event.target.value},
-      
-      
-    );
-  }
-
-  onNumberChange = (event) =>{
-    this.setState({number: event.target.value})
-  }
-  backgroundColorChange =(event) =>{
-    this.setState({color:event.target.value})
-  }
-  
   render() {
     return (
       <form>
-      <h1>Hello {this.state.username}</h1>
-      <h2>number is {this.state.number} </h2>
-      <p>Enter your name:</p>
-      <input type='text' onChange={this.myChangeHandler} /><br />
-      <label htmlFor="name">Name</label>
-      <input type="text" /><br />
-      <label htmlFor="password">Password</label>
-      <input type="password" /> <br />
-      <label htmlFor="number">Number</label>
-      <input type="number" onChange ={this.onNumberChange} /><br />
-      <label htmlFor="date"> Start Date</label>
-      <input type="date" /><br />
-      <label htmlFor="endDate">End Date</label>
-      <input type="date" /> <br />
-      <input type="color" onChange={this.state.backgroundColorChange}/>
-
-      <input type="submit"  />
-
+      <textarea cols= "60" rows =" 40"
+       value={this.state.description} />
       </form>
     );
   }
